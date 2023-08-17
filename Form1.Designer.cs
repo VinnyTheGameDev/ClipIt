0381bf13a41618df25182b59f85003a6371dcb8c
Vlux <77426686+VinnyTheGameDev@users.noreply.github.com>
2023-08-16 22:27:57 -0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace ClipIt
+{
+    partial class Window
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CaptureScreenButton = new System.Windows.Forms.Button();
+            this.ScreenCapture = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ScreenCapture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CaptureScreenButton
+            // 
+            this.CaptureScreenButton.Location = new System.Drawing.Point(846, 682);
+            this.CaptureScreenButton.Name = "CaptureScreenButton";
+            this.CaptureScreenButton.Size = new System.Drawing.Size(200, 50);
+            this.CaptureScreenButton.TabIndex = 0;
+            this.CaptureScreenButton.Text = "Clip It!";
+            this.CaptureScreenButton.UseVisualStyleBackColor = true;
+            // 
+            // ScreenCapture
+            // 
+            this.ScreenCapture.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.ScreenCapture.Location = new System.Drawing.Point(12, 12);
+            this.ScreenCapture.Name = "ScreenCapture";
+            this.ScreenCapture.Size = new System.Drawing.Size(1034, 609);
+            this.ScreenCapture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ScreenCapture.TabIndex = 1;
+            this.ScreenCapture.TabStop = false;
+            // 
+            // Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1058, 744);
+            this.Controls.Add(this.ScreenCapture);
+            this.Controls.Add(this.CaptureScreenButton);
+            this.Name = "Window";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ScreenCapture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CaptureScreenButton;
+        private System.Windows.Forms.PictureBox ScreenCapture;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+using System.Windows.Interop;
+
+namespace ClipIt
+{
+
+    public partial class Window : Form
+    {
+
+        public Window()
+        {
+
+            InitializeComponent();
+
+        }
+
+        void Capture()
+        {
+
+            while (true)
+            {
+
+                Bitmap bitmap = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+                Graphics graphics = Graphics.FromImage(bitmap);
+                graphics.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+                ScreenCapture.Image = bitmap;
+
+                Thread.Sleep(1000);
+
+            }
+
+        }
+
+        private void ClipButton_Click(object sender, EventArgs e)
+        {
+
+            Thread thread = new Thread(() => Capture());
+            thread.Start();
+
+        }
+
+    }
+
+}
